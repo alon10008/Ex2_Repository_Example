06430b97b6ee3a5448cee21659fc6bdf063d443f
Yael Landau <yaelandau22@gmail.com>
2020-12-11 15:10:22 +0200

    add test

---
+++ test/tests_DWGraph_DS.java
@@ -0,0 +1,15 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class tests_DWGraph_DS 
+{
+    @Test
+    public void stamTest()
+	{
+        assertNull(null);
+    }
+
+}
